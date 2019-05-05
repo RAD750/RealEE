3403a1d610470747fdbb932ded10715d525e613b
lor54 <42451244+lor54@users.noreply.github.com>
2019-05-05 21:41:46 +0200

    Updated to v1.2.5

--- proxies/ServerProxy.java
+++ proxies/ServerProxy.java
@@ -8,16 +8,26 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-public class CommonProxy
+public class ServerProxy
+  extends CommonProxy
 {
-  public CommonProxy() {}
+  public ServerProxy() {}
   
   @Mod.PreInit
-  public void preInit(FMLPreInitializationEvent event) {}
+  public void preInit(FMLPreInitializationEvent event)
+  {
+    super.preInit(event);
+  }
   
   @Mod.Init
-  public void init(FMLInitializationEvent event) {}
+  public void init(FMLInitializationEvent event)
+  {
+    super.init(event);
+  }
   
   @Mod.PostInit
-  public void postInit(FMLPostInitializationEvent event) {}
+  public void postInit(FMLPostInitializationEvent event)
+  {
+    super.postInit(event);
+  }
 }
